424c417a08ed0e5fdd07748d91dbbeb43d3886ad
Marco Borst <marco@splatter.nl>
2019-12-03 14:48:29 +0100

    catching up on day 1 part 2

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,8 @@ import aoc._
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("D01 : " + Day01.result)
+    println("D01/1: " + Day01.result1)
+    println("D01/2: " + Day01.result2)
   }
 
 }
--- src/main/scala/aoc/Day01.scala
+++ src/main/scala/aoc/Day01.scala
@@ -19,14 +19,31 @@ object Day01 {
 
   assert(masses.length == 100)
 
-  def massToFuel(m: Int): Int =
+  // Part 1
+
+  def massToFuel1(m: Int): Int =
     (m / 3) - 2
 
-  assert(massToFuel(12)     == 2)
-  assert(massToFuel(14)     == 2)
-  assert(massToFuel(1969)   == 654)
-  assert(massToFuel(100756) == 33583)
+  assert(massToFuel1(12)     == 2)
+  assert(massToFuel1(14)     == 2)
+  assert(massToFuel1(1969)   == 654)
+  assert(massToFuel1(100756) == 33583)
 
-  val result: Int =
-    masses.map(massToFuel).sum
+  val result1: Int =
+    masses.map(massToFuel1).sum
+
+  // Part 2
+
+  def massToFuel2(m: Int): Int = {
+    val f = (m / 3) - 2
+    if (f <= 0) 0 else f + massToFuel2(f)
+  }
+
+  assert(massToFuel2(12)     == 2)
+  assert(massToFuel2(1969)   == 966)
+  assert(massToFuel2(100756) == 50346)
+    
+  val result2: Int =
+    masses.map(massToFuel2).sum
+
 }
