91910c74fa6c690e505f2b1a5fc2471aeb9558ec
nmcb <nmcb@xs4all.nl>
2025-06-01 13:07:48 +0200

    cleanup day 01

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -12,21 +12,24 @@ object Day01 extends App:
       .map(_.toInt)
       .toVector
 
-  def massToFuel1(mass: Int): Int =
+  def massToFuel(mass: Int): Int =
     (mass / 3) - 2
 
   val start1  = System.currentTimeMillis
-  val answer1 = masses.map(massToFuel1).sum
+  val answer1 = masses.map(massToFuel).sum
   println(s"Day $day answer part 1: $answer1 [${System.currentTimeMillis - start1}ms]")
 
 
-  def massToFuel2(mass: Int): Int =
+  def massToFuelRequirement(mass: Int): Int =
     @tailrec
-    def go(mass: Int, result: Int = 0): Int =
-      val f = (mass / 3) - 2
-      if f <= 0 then result else go(f - 1, result + f)
+    def go(mass: Int, total: Int = 0): Int =
+      val requirement = massToFuel(mass)
+      if requirement <= 0 then
+        total
+      else
+        go(requirement - 1, total + requirement)
     go(mass)
 
-  val start2 = System.currentTimeMillis
-  val answer2 = masses.map(massToFuel2).sum
-  println(s"Day $day answer part 1: $answer2 [${System.currentTimeMillis - start1}ms]")
+  val start2  = System.currentTimeMillis
+  val answer2 = masses.map(massToFuelRequirement).sum
+  println(s"Day $day answer part 2: $answer2 [${System.currentTimeMillis - start1}ms]")
