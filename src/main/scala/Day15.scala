8a1efeec1cb7828b8679ed1e1297a70f54028ca9
nmcb <nmcb@xs4all.nl>
2025-06-06 12:22:43 +0200

    solved day 15

--- src/main/scala/Day15.scala
+++ src/main/scala/Day15.scala
@@ -13,11 +13,9 @@ object Day15 extends App:
   val neighbours: Vector[(Pos,Int)] =
     Vector(Pos(0, -1) -> 1, Pos(0, 1) -> 2, Pos(-1, 0) -> 3, Pos(1, 0) -> 4)
 
-  case class Tile(pos: Pos, cpu: CPU, status: Long)
-
-  def move(pos: Pos, cpu: CPU)(delta: Pos, command: Int): Tile =
+  def move(pos: Pos, cpu: CPU)(delta: Pos, command: Int): (Pos,CPU,Long) =
     val (next, status) = cpu.withInput(command).outputStates.last
-    Tile(pos + delta, next, status)
+    (pos + delta, next, status)
 
   def dijkstra(cpu: CPU): (Map[Pos,Int], Option[(Pos,CPU)]) =
     val steps = mutable.Map(Pos(0,0) -> 0)
@@ -28,12 +26,12 @@ object Day15 extends App:
       val (from, cpu) = todo.dequeue
       neighbours
         .map(move(from, cpu))
-        .filter(tile => !steps.contains(tile.pos) || steps(from) + 1 < steps(tile.pos))
-        .filter(_.status > 0)
-        .foreach: tile =>
-          steps(tile.pos) = steps(from) + 1
-          todo.enqueue(tile.pos -> cpu)
-          if tile.status == 2 then target = Some(tile.pos -> cpu)
+        .filter((next,_,_) => !steps.contains(next) || steps(from) + 1 < steps(next))
+        .filter((_,_,status) => status > 0)
+        .foreach: (next, cpu, status) =>
+          steps(next) = steps(from) + 1
+          todo.enqueue(next -> cpu)
+          if status == 2 then target = Some(next -> cpu)
 
     (steps.toMap, target)
 
@@ -41,17 +39,17 @@ object Day15 extends App:
     val (first, Some(target, _)) = dijkstra(CPU(program)): @unchecked
     first(target)
 
+  def solve2(program: Mem): Int =
+    val (_, Some(_, computer)) = dijkstra(CPU(program)): @unchecked
+    val (second, _) = dijkstra(computer)
+    second.values.max
+
+
   val program: Mem = Mem.parse(Source.fromResource(s"input$day.txt").mkString.trim)
 
   val start1  = System.currentTimeMillis
   val answer1 = solve1(program)
   println(s"Day $day answer part 1: $answer1 [${System.currentTimeMillis - start1}ms]")
-
-  
-  def solve2(memory: Mem): Int =
-    val (_, Some(_, computer)) = dijkstra(CPU(memory)): @unchecked
-    val (second, _) = dijkstra(computer)
-    second.values.max
 
   val start2  = System.currentTimeMillis
   val answer2 = solve2(program)
